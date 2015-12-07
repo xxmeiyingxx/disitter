a2e82f613d881cfd3d44c17cd06889b3685f9d1c
Sadahiro Ikeuchi <meiying.ruby@gmail.com>
2015-12-07 05:26:52 +0000

    Finish static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+
   test "should get home" do
     get :home
     assert_response :success
@@ -11,4 +12,8 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
+  test "should get about" do
+    get :about
+    assert_response :success
+  end
 end
