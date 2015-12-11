f767b5fb779d965772ab2a13877448eb0d545191
Sadahiro Ikeuchi <meiying.ruby@gmail.com>
2015-12-11 02:59:09 +0000

    login logout

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -24,6 +24,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                             password_confirmation: "password" }
     end
     assert_template 'users/show'
-    assert_not flash.FILL_IN
+    assert is_logged_in?
   end
 end
