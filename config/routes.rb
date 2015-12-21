ceab8601d084c83095ee98b04ada7e690ae85a6b
Sadahiro Ikeuchi <meiying.ruby@gmail.com>
2015-12-21 02:14:27 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -23,10 +23,15 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 
   # Example resource route with options:
   #   resources :products do
